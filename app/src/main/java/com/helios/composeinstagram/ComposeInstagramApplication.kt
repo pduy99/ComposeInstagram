3862a22cba9c9ec8135fbf98653db958f40ac843
pduy99 <1712018@student.hcmus.edu.vn>
2023-10-09 15:41:00 +0700

    Init project structure

---
+++ app/src/main/java/com/helios/composeinstagram/ComposeInstagramApplication.kt
@@ -0,0 +1,9 @@
+package com.helios.composeinstagram
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ComposeInstagramApplication : Application() {
+    // TODO: Init something here
+}
